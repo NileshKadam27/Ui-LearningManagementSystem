514ecd9a4ebef5228247abbe9e8b4e340d2019eb
garry278 <gauravpawar26224@gmail.com>
2024-10-14 13:56:46 +0530

    chnages

--- src/app/components/trendingcourses/trendingcourses.component.ts
+++ src/app/components/trendingcourses/trendingcourses.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfessorService } from '../../services/professor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-trendingcourses',
@@ -7,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './trendingcourses.component.html',
   styleUrl: './trendingcourses.component.css'
 })
-export class TrendingcoursesComponent {
+export class TrendingcoursesComponent implements OnInit {
+trendingCourses: any;
 
+
+constructor(private profService:ProfessorService,private route :Router){
+
+}
+  ngOnInit(): void {
+this.getCourses()  }
+
+
+getCourses() {
+
+  this.profService.getCourseListWithouLogin().subscribe((res) => {
+    this.trendingCourses = res.payload;
+    this.trendingCourses=this.trendingCourses.slice(0,4)
+
+  });
+}
+
+
+startLearning(id:any){
+
+  this.route.navigate(['/coursedetails'], {
+    queryParams: { courseId: id },
+  });
+}
 }
